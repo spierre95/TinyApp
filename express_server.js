768e7222aec1d0cfd1c8566c3db8ffc6c95e48ac
spierre95 <stephen.pierre20@gmail.com>
2018-04-17 20:51:38 +0000

    added partials directory, header file, and urls_show.ejs for diplaying shortened urls.

--- express_server.js
+++ express_server.js
@@ -30,3 +30,8 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
